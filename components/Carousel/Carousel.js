73c984777ec97e1a309c58651569246997a04803
maks112v <maks112v@gmail.com>
2019-02-01 12:22:58 -0500

    working on stretch

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,9 +1,22 @@
 class Carousel {
-
+    constructor(atr){
+        this.btn = atr;
+        this.btn.addEventListener('click', () => this.update() );
+    }
+    update(){
+        //console.log('this');
+    }
 }
 
-let carousel = document.querySelector();
+let carousel = document.querySelector('.carousel');
 
+let carouselBtn = carousel.querySelectorAll('div');
+
+carouselBtn.forEach( btn => new Carousel(btn));
+
+const carouselImg = [];
+
+Array.from(carousel.querySelectorAll('img')).forEach(img => carouselImg.push([((img.style.display === "flex")? true: false), img]));
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
     2. You will need to grab a reference to all of the images
